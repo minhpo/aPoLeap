5a50b9f6a5d83486caddc2af1cb5a5ab0e77290a
Po Sam <minhpo@gmail.com>
2013-09-23 21:59:34 +0200

    Initial Commit

---
+++ aPoLeap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  aPoLeap
+//
+//  Created by Po Sam | The Mobile Company on 9/23/13.
+//  Copyright (c) 2013 Po Sam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
